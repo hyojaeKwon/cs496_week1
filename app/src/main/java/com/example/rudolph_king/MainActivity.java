fcb7961a96cee74328888be4fa6faba00add7bde
hyojaeKwon <khj010909@gm.gist.ac.kr>
2021-12-28 18:32:20 +0900

    [add] make tab

--- app/src/main/java/com/example/rudolph_king/MainActivity.java
+++ app/src/main/java/com/example/rudolph_king/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.rudolph_king;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+
+
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager vp = findViewById(R.id.viewpager);
+        VPAdapter adapter;
+        adapter = new VPAdapter(getSupportFragmentManager());
+        vp.setAdapter(adapter);
+
+        TabLayout tab = findViewById(R.id.tab);
+        tab.setupWithViewPager(vp);
+
+        ArrayList<Integer> images = new ArrayList<>();
+//        images.add(R.drawable);
+//        images.add(R.drawable.sea);
+//        images.add(R.drawable.set);
+//
+//        for(int i = 0 ; i < 3 ; i++){
+//            tab.getTabAt(i).setIcon(images.get(i));
+//        }
     }
 }
---
+++ app/src/main/java/com/example/rudolph_king/VPAdapter.java
@@ -0,0 +1,44 @@
+package com.example.rudolph_king;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class VPAdapter extends FragmentPagerAdapter {
+    private ArrayList<Fragment> items;
+    private ArrayList<String> itext = new ArrayList<String>();
+
+    public VPAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+        items = new ArrayList<Fragment>();
+        items.add(new Fragment1());
+        items.add(new Fragment2());
+        items.add(new Fragment3());
+
+        itext.add("List");
+        itext.add("Gallery");
+        itext.add("Map");
+
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position){
+        return itext.get(position);
+    }
+}
