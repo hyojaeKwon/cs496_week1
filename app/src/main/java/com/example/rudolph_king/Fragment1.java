3f956789f5089507373103b0cd28a5d516eaf6b5
hyojaeKwon <khj010909@gm.gist.ac.kr>
2021-12-29 20:47:01 +0900

    [merge] test and master

--- app/src/main/java/com/example/rudolph_king/Fragment1.java
+++ app/src/main/java/com/example/rudolph_king/Fragment1.java
@@ -130,28 +130,4 @@ public class Fragment1 extends Fragment {
         return view;
     }
 
-    //data class
-//    class Actor {
-//        private String name;
-//        private String summary;
-//        private String thumb_url;
-//
-//        public Actor(String name, String thumb_url, String summary) {
-//            this.name = name;
-//            this.summary = summary;
-//            this.thumb_url = thumb_url;
-//        }
-//
-//        public String getName() {
-//            return name;
-//        }
-//
-//        public String getSummary() {
-//            return summary;
-//        }
-//
-//        public String getThumb_url() {
-//            return thumb_url;
-//        }
-//    }
 }
