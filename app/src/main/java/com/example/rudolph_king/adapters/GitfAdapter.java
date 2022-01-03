062cb0d17f20f4dc1b6a00969bbf36b39b5602d2
hyojaeKwon <khj010909@gm.gist.ac.kr>
2022-01-03 14:52:34 +0900

    make fr3 row item layout

--- app/src/main/java/com/example/rudolph_king/adapters/GitfAdapter.java
+++ app/src/main/java/com/example/rudolph_king/adapters/GitfAdapter.java
@@ -1,19 +1,24 @@
 package com.example.rudolph_king.adapters;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.rudolph_king.Gift;
 import com.example.rudolph_king.R;
 import com.example.rudolph_king.Shops;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class GitfAdapter extends RecyclerView.Adapter<GitfAdapter.ItemViewHolder> {
@@ -44,7 +49,7 @@ public class GitfAdapter extends RecyclerView.Adapter<GitfAdapter.ItemViewHolder
 
     @Override
     public void onBindViewHolder(@NonNull GitfAdapter.ItemViewHolder holder, int position) {
-        holder.onBind(giftData.get(position));
+        holder.onBind(giftData.get(position),position);
     }
 
     @Override
@@ -57,22 +62,37 @@ public class GitfAdapter extends RecyclerView.Adapter<GitfAdapter.ItemViewHolder
 
     class ItemViewHolder extends RecyclerView.ViewHolder{
         private TextView tv1;
-        private TextView tv2;
-        private TextView tv3;
+        private TextView tv_price;
+        private TextView tv_company;
+        private ImageView iv_picture;
+        private TextView tag_number;
 
         //ItemViewHolder의 생성자
         ItemViewHolder(View view){
             super(view);
-
-            tv1 = view.findViewById(R.id.tv_ranking);
-
-
+            tag_number = view.findViewById(R.id.tv_ranking);
+            tv_company = view.findViewById(R.id.tv_company);
+            tv1 = view.findViewById(R.id.tv_productName);
+            tv_price = view.findViewById(R.id.tv_price);
+            iv_picture = view.findViewById(R.id.imageView_product);
         }
 
 
         //상품 이름 집어넣는 코드
-        void onBind(Gift gift){
+        void onBind(Gift gift,int index){
+            index = index + 1;
+            tag_number.setText("# " + Integer.toString(index));
+            tv_company.setText(gift.getCompany());
             tv1.setText(gift.getProductName());
+            int price = gift.getPrice();
+            DecimalFormat df = new DecimalFormat("#,###");
+            tv_price.setText(df.format(price)+"원");
+            Glide
+                    .with(context)
+                    .load(gift.getPictureUrl())
+                    .centerCrop()
+                    .apply(new RequestOptions().override(250, 250))
+                    .into(iv_picture);
         }
     }
 }
